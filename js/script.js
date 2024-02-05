c53f01ea85930c350e48c541a8247eb20ddbb94e
guessgame <tamimh0727@gmail.com>
2024-02-05 12:56:49 +0600

    practice1

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.getElementById("demo").innerHTML = "hi"
+
+document.getElementById("two").innerHTML = 'how are you'
+
+window.alert("warning")
+
+let x = 10;
+let y = 30;
+
+
+
+
+
+
+console.log(x<y);
+
+// console.log(y + x);
